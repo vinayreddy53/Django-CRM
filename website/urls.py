a08dfd2cab30a57c70f5645982cdf3f1aa0cb35d
Vinay Reddy <reddyvinu053@gmail.com>
2023-10-16 12:41:33 +0530

    'Registration'

---
+++ website/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'), 
+    path('register/', views.register_user, name='register'), 
+]
